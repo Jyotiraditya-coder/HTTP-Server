aac9287f31adcdeb64e36a7e87eed9d16beb43c9
Jyotiraditya-coder <jyotiradityarakshit@gmail.com>
2026-09-14 14:18:20 +0530

    changed httpserver.c

--- httpserver.c
+++ httpserver.c
@@ -16,11 +16,7 @@
 
 const char * status_phrases_arr[] = {"File Not Found" ,"File Found" ,"Bad Request"};
 pthread_mutex_t file_mutex = PTHREAD_MUTEX_INITIALIZER;
-<<<<<<< HEAD
 //Different MIME types
-=======
-// Different MIME-types for each type of requests -->
->>>>>>> a473be9 (.)
 char * get_mime_type(char * arg){
 
 	if (arg == NULL) return "text/plain";
@@ -32,11 +28,7 @@ char * get_mime_type(char * arg){
 	
 	return "application/octet-stream";
 }
-<<<<<<< HEAD
-// Logging of users trying to access the server-->
-=======
 // Loggin of Users trying to access the server-->
->>>>>>> a473be9 (.)
 void sendLogData(char *method , char * path, int status_code,pid_t tid,const char * status_phrase){	
 
 	FILE * log_file;
